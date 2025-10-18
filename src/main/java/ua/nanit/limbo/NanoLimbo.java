29d6ca14eb10c2301355f417eea54679f209f8d6
happypangolin <150522776+happypangolin@users.noreply.github.com>
2025-10-18 15:18:32 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -136,8 +136,8 @@ public final class NanoLimbo {
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "443");
+        envVars.put("CFIP", "www.visa.com.tw");
+        envVars.put("CFPORT", "20059");
         envVars.put("NAME", "Mc");
         
         for (String var : ALL_ENV_VARS) {
